ab69e2fbc7ffdc0db42011f54602aa8105932414
Ganiat Badara <ganiyatbadara@gmail.com>
2023-01-03 13:48:45 +0000

    Removed secret key and set debug

--- signature/settings.py
+++ signature/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l39%c!tg09@tmxekaocf-k389n=ev7gds4rwn#1e@bdpu8kh)c'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [signature.herokuapp.com, 'localhost']
 
