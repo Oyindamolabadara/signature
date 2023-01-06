4edeca7f07272fe7825d722e4f090a42d54f3f5a
Ganiat Badara <ganiyatbadara@gmail.com>
2023-01-06 04:27:09 +0000

    Create blog app structure and views

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='blog'),
+    path('<slug:slug>/', views.BlogDetail.as_view(), name='blog_detail'),
+    # path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
+]
--- signature/urls.py
+++ signature/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('summernote/', include('django_summernote.urls')),
-    # path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'signature.views.handler404'
