1695e52d7a7b057c3b6d95c0714a97b36e507ca0
Ganiat Badara <ganiyatbadara@gmail.com>
2023-01-29 22:05:23 +0000

    Add sitemap and robots.txt files

--- company_info/views.py
+++ company_info/views.py
@@ -25,32 +25,31 @@ def shipping_returns(request):
 
 
 def contact(request):
-	
-	if request.method == 'POST':
-	    first_name = request.POST.get('first_name')
-	    last_name = request.POST.get('last_name')
-	    subject = request.POST.get('subject')
-	    email_address = request.POST.get('email_address')
-	    message = request.POST.get('message')
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        subject = request.POST.get('subject')
+        email_address = request.POST.get('email_address')
+        message = request.POST.get('message')
 
-	    message_data = {
-			'first_name': first_name,
-			'last_name': last_name,
-			'email_address': email_address,
-			'subject': subject,
-			'message': message,
-		}
-	    message = '''
-		From: {}
-		New message: {}
-		'''.format(message_data['email_address'], message_data['message'])
+        message_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email_address': email_address,
+            'subject': subject,
+            'message': message,
+        }
+        message = '''
+            From: {}
+            New message: {}
+            '''.format(message_data['email_address'], message_data['message'])
 
-	    send_mail(
-			message_data['subject'], message, '', ['ganiyatbadara@gmail.com'])
+        send_mail(
+            message_data['subject'], message, '', ['ganiyatbadara@gmail.com'])
 
-	    messages.info(request, (
-			f'Your message has been sent, we will contact you \
-				via { email_address } as soon as possible.'))
-	return render(request, 'home/index.html')
+        messages.info(request, (
+            f'Your message has been sent, we will contact you \
+            via { email_address } as soon as possible.'))
+        return render(request, 'home/index.html')
 
-	return render(request, 'contact_us/contact_us.html')
+    return render(request, 'contact_us/contact_us.html')
